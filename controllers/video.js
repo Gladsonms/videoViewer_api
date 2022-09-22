8e370e56842097a6d68dc097efbc05082292186b
gladsonms <gladsonsaji4@gmail.com>
2022-09-22 09:52:30 +0530

    added update video route

--- controllers/video.js
+++ controllers/video.js
@@ -1,3 +1,5 @@
+import { CreateError } from "../middlewares/errorHandlingMiddleware";
+import Video from "../modals/Video";
 import Video from "../modals/Video";
 
 //Add video
@@ -6,6 +8,26 @@ export const AddVideo = async (req, res, next) => {
   try {
     const savedVideo = await newVideo.save();
     res.status(200).json({ savedVideo });
+  } catch (error) {
+    next(error);
+  }
+};
+
+//Upadte  Video
+export const updateVideo = async (req, res, next) => {
+  try {
+    const video = await Video.findById(req.params.id);
+    if (!video) return next(CreateError(404, "Video not found"));
+    if (req.user.id === Video.userId) {
+      const updatedVideo = await Video.findByIdAndUpdate(
+        req.params.id,
+        {
+          $set: req.body,
+        },
+        { new: true }
+      );
+    }
+    res.status(200).json(updatedVideo);
   } catch (error) {
     next(error);
   }
--- routes/video.js
+++ routes/video.js
@@ -1,4 +1,5 @@
 import express from "express";
+import { AddVideo, updateVideo } from "../controllers/video";
 import { verifyToken } from "../Utlis/verifyToken";
 
 const router = express.Router();
@@ -7,6 +8,12 @@ const router = express.Router();
 //Route Post /video
 //@acess private
 
-router.post("/", verifyToken);
+router.post("/", verifyToken, AddVideo);
+
+//@desc update video
+//Route Put /Video
+//@acess Private
+
+router.put("/", verifyToken, updateVideo);
 
 export default router;
