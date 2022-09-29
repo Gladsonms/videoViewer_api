e6d78018a64cf80d1b94a972f22c3e718f3e9508
gladsonms <gladsonsaji4@gmail.com>
2022-09-29 10:26:02 +0530

    delete video comment and get all comments

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,10 +1,39 @@
+import { CreateError } from "../middlewares/errorHandlingMiddleware";
 import Comments from "../modals/Comments";
+import Video from "../modals/Video.js";
 
+//add comment
 export const addComment = async (req, res, next) => {
   const newComment = new Comment({ ...req.body, userId: req.user.id });
   try {
     const savedComment = await Comments.save();
     res.status(200).json(savedComment);
+  } catch (error) {
+    next(error);
+  }
+};
+
+//delete comment
+export const deleteComment = async (req, res, next) => {
+  try {
+    const comment = await Comments.findById(res.params.id);
+    const video = await Video.findById(res.params.id);
+    if (req.user.id === comment.userId || req.user.id === video.userId) {
+      await Comments.findByIdAndDelete(req.params.id);
+      res.status(200).json("The comment deleted succefully");
+    } else {
+      return next(CreateError(403, "You can delete only your comment"));
+    }
+  } catch (error) {
+    next(error);
+  }
+};
+
+//get all comments
+export const getComments = async (req, res, next) => {
+  try {
+    const comments = await Comments.find({ videoId: req.params.videoId });
+    res.status(200).json(comments);
   } catch (error) {
     next(error);
   }
--- routes/comment.js
+++ routes/comment.js
@@ -1,8 +1,21 @@
 import express from "express";
+import { addComment, deleteComment, getComments } from "../controllers/comment";
 import { verifyToken } from "../Utlis/verifyToken";
 
 const router = express.Router();
 
-router.post("/", verifyToken);
+// @desc   add comment
+// @route POST /comment
+// @access Private
+router.post("/", verifyToken, addComment);
 
+// @desc   delete  comment
+// @route DELETE /comment/:id
+// @access Private
+router.delete("/:id", verifyToken, deleteComment);
+
+// @desc   get comments
+// @route GET /video
+// @access Public
+router.get("/", getComments);
 export default router;
